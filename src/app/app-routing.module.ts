be2a9e777e95fa1f580db56ae5414ed36ac28316
Dalphin <dalphinjesu@gmail.com>
2019-10-06 02:32:59 +0530

    Theme layout header and left menu has added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ThemeLayoutComponent } from './theme-layout/theme-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ThemeLayoutComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/theme-layout/theme-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'theme-layout',
+  templateUrl: './theme-layout.component.html',
+  styleUrls: ['./theme-layout.component.css']
+})
+export class ThemeLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
